d8b53925b8f587d5c8150f3f659b28eac4502aae
Brendan O'Dea <brendan.ode4@gmail.com>
2018-03-22 20:04:02 +0000

    automatically backed up by learn

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,9 +1,9 @@
 class Helpers
   def self.current_user(session_hash)
-    session_hash[session[:id]]
+    User.find_by(name: [session[:id])
   end
 
   def self.is_logged_in?(session_hash)
-    !session_hash[session[:id]]
+    !session_hash[:id]
   end
 end
