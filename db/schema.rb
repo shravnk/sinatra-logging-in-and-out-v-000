b6b27935c0ea3532a75cc81ab09f1ddecb1f3836
Brendan O'Dea <brendan.ode4@gmail.com>
2018-03-22 20:23:37 +0000

    automatically backed up by learn

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180322194102) do
-
-  create_table "users", force: :cascade do |t|
-    t.string "name"
-    t.string "password"
-    t.decimal "balance"
-  end
+ActiveRecord::Schema.define(version: 0) do
 
 end
