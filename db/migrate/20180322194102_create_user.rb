24bd6fc5ef3e003727a7888cb9215fd3ba31d445
Brendan O'Dea <brendan.ode4@gmail.com>
2018-03-22 20:23:20 +0000

    automatically backed up by learn

--- db/migrate/20180322194102_create_user.rb
+++ db/migrate/20180322194102_create_user.rb
@@ -1,7 +1,7 @@
 class CreateUser < ActiveRecord::Migration[5.1]
   def change
     create_table :users do |t|
-      t.string :name
+      t.string :username
       t.string :password
       t.decimal :balance
     end
